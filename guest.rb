e3774403bfb3e5e4a7b872e7e13b333a865e1718
Antonio Lorusso <mitictonylor@gmail.com>
2020-03-20 16:29:39 +0000

    add add_favourite_song, check_favourite_song_size,pay_room function to the guest

--- guest.rb
+++ guest.rb
@@ -9,4 +9,21 @@ attr_reader :name, :favourite_songs, :money, :age
 
   end
 
+def check_favourite_songs_size()
+  return @favourite_songs.size()
+end
+
+def add_favourite_song(song)
+  @favourite_songs.push(song)
+end
+
+def pay_room(room)
+  @money -= room.fee
+end
+
+
+
+
+
+
 end
